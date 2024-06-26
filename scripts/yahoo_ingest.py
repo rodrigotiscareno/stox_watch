4dde6defb8bccd219c5f3ca39399a7cf701fbeae
rodrigotiscareno <rodrigotiscareno1@gmail.com>
2024-06-26 09:47:29 -0400

    yahoo changes

--- scripts/yahoo_ingest.py
+++ scripts/yahoo_ingest.py
@@ -6,32 +6,57 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    # ticker_information = {}
-    # all_prices = []
-
+    ticker_information = {}
+    all_prices = []
+    all_recs = []
+    all_insider_summaries = []
     recommendations = {}
+
     for ticker in get_tickers():
         current_ticker = yf.Ticker(ticker)
 
-        df = current_ticker.upgrades_downgrades
+        insider_df = current_ticker.insider_purchases
+        insider_df.columns = insider_df.columns.str.lower().str.replace(" ", "_")
+        insider_df = insider_df.pivot_table(
+            index=None,
+            columns="insider_purchases_last_6m",
+            values=["shares", "trans"],
+            aggfunc="first",
+        )
+        insider_df.columns = insider_df.columns.str.lower().str.replace(" ", "_")
+        insider_df = insider_df.rename(columns={"insider_purchases_last_6m": "type"})
+        insider_df.columns.name = None
+        insider_df = insider_df.reset_index(names=["type"])
+        insider_df["ticker"] = ticker
 
-        df = df.loc[[df.GradeDate > "2024-01-01"]]
-        recommendations[ticker] = current_ticker.upgrades_downgrades
+        current_rec_df = current_ticker.upgrades_downgrades
+        current_rec_df.index = pd.to_datetime(current_rec_df.index)
+        current_rec_df = current_rec_df[
+            current_rec_df.index > "2024-01-01"
+        ].reset_index(drop=False)
+        current_rec_df["ticker"] = ticker
 
-    print(recommendations)
+        ticker_information[ticker] = current_ticker.info
 
-    # ticker_information[ticker] = current_ticker.info
+        current_price_df = current_ticker.history(period="5y", interval="1d")
+        current_price_df["ticker"] = ticker
 
-    # current_price_df = current_ticker.history(period="5y", interval="1d")
-    # current_price_df["ticker"] = ticker
+        all_prices.append(current_price_df)
+        all_recs.append(current_rec_df)
+        all_insider_summaries.append(insider_df)
 
-    # all_prices.append(current_price_df)
+    prices_df = pd.concat(all_prices)
+    prices_df.reset_index(inplace=True, drop=False)
+    prices_df.to_csv("prices.csv")
 
-    # prices_df = pd.concat(all_prices)
-    # prices_df.reset_index(inplace=True, drop=False)
-    # prices_df.to_csv("prices.csv")
+    recs_df = pd.concat(all_recs)
+    recs_df.reset_index(inplace=True, drop=False)
+    recs_df.to_csv("recs.csv")
 
-    # info_df = pd.DataFrame.from_dict(ticker_information, orient="index")
-    # info_df.reset_index(inplace=True)
-    # info_df.rename(columns={"index": "ticker"}, inplace=True)
-    # info_df.to_csv("ticker_information.csv", index=False)
+    info_df = pd.DataFrame.from_dict(ticker_information, orient="index")
+    info_df.reset_index(inplace=True)
+    info_df.rename(columns={"index": "ticker"}, inplace=True)
+    info_df.to_csv("ticker_information.csv", index=False)
+
+    insider_df = pd.concat(all_insider_summaries)
+    insider_df.to_csv("insider_summaries.csv", index=False)
