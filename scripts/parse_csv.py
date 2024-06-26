fdcb27252ef5c7b049d453e19e9dde2e59ed1101
rodrigotiscareno <rodrigotiscareno1@gmail.com>
2024-06-26 11:09:18 -0400

    enhance parse script

--- scripts/parse_csv.py
+++ scripts/parse_csv.py
@@ -6,7 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def load_csv_to_sql(csv_file_path, table_name):
+def load_csv_to_sql(csv_file_path, table_name, mode="replace"):
+    """
+    Load data from a CSV file to a SQL table.
+
+    Parameters:
+    csv_file_path (str): Path to the CSV file.
+    table_name (str): Name of the SQL table.
+    mode (str): Valid options are:
+                'replace' - Drop the table before inserting new values.
+                'append' - Insert new values into the existing table.
+                'fail' - Raise a ValueError if the table already exists.
+    """
     username = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
     host = os.getenv("DB_HOST")
@@ -18,8 +29,7 @@ def load_csv_to_sql(csv_file_path, table_name):
     )
 
     data = pd.read_csv(csv_file_path)
-    data.to_sql(table_name, con=engine, index=False, if_exists="replace")
+    data.to_sql(table_name, con=engine, index=False, if_exists=mode)
 
 
-load_csv_to_sql("prices.csv", "ticker_price")
-load_csv_to_sql("sentiments.csv", "sentiments")
+load_csv_to_sql("summary_detail.csv", "summary_detail", mode="replace")
