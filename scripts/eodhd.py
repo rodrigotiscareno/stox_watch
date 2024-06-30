ee10fef75ae50b4b5aaa19814140eecd4f19cf5d
Michael Sheng <m3sheng@uwaterloo.ca>
2024-06-30 13:19:32 -0400

    new eodhd key

--- scripts/eodhd.py
+++ scripts/eodhd.py
@@ -11,12 +11,14 @@ def fetch_and_save_sentiments():
 
     lowercase_ticker_list = [ticker.lower() for ticker in get_tickers(100)]
     result_string = ','.join(lowercase_ticker_list)
-    endpoint = f'https://eodhd.com/api/sentiments?s={result_string}&api_token=66746b7e20e902.29520100&fmt=json'
+    key = "6681931e767dc4.06305877"
+    endpoint = f'https://eodhd.com/api/sentiments?s={result_string}&api_token={key}&fmt=json'
 
     response = requests.get(endpoint)
 
     if response.status_code == 200:
         sentiments = response.json()  # Parse JSON response
+        print(sentiments)
     else:
         print(f"Failed to retrieve data: {response.status_code}, {response.text}")
         
@@ -28,22 +30,19 @@ def fetch_and_save_sentiments():
     with open(csv_file, mode='w', newline='') as file:
         writer = csv.writer(file)
         
-        # Write the header
         writer.writerow(["company", "date", "count", "normalized"])
         
-        # Write the rows for each company
         for company, records in data_to_write.items():
             for entry in records:
                 writer.writerow([company, entry["date"], entry["count"], entry["normalized"]])
 
     print(f"Data has been written to {csv_file}")
-    
-    load_csv_to_sql("sentiments.csv", "sentiments")
-        
 
 
 if __name__ == "__main__":
     fetch_and_save_sentiments()
+    load_csv_to_sql("sentiments.csv", "sentiments")
+
 
 
 
