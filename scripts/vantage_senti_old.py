3b0c57cb1b98ad0401708b99a3c1957d7827969a
ineshjacob <ineshjacob@gmail.com>
2024-07-07 18:54:53 -0400

    updating_senti

--- scripts/vantage_senti_old.py
+++ scripts/vantage_senti_old.py
@@ -33,57 +33,47 @@ api_key = "9NLUTD6I2QZTR2BZ"
 
 # function to get sentiment and news for a given symbol
 def fetch_sentiment(symbol):
-    url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={symbol}&time_from={time_from}&limit=1000&apikey={api_key}"
+    url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={symbol}&time_from={time_from}&apikey={api_key}"
     response = requests.get(url)
     data = response.json()
     return data
-
-# function to convert time string to datetime object
-def convert_to_datetime(time_str):
-    return datetime.strptime(time_str, "%Y%m%dT%H%M%S")
 
 
 def fetch_and_save_sentiment():
     # fetch the top 100 symbols.
     tickers = get_tickers()
-
     sentiments = []
-    list_tickers = []
-    time_published = []
 
-    # retriving sentiment for each ticker
     for symbol in tickers:
         data = fetch_sentiment(symbol)
+        average_sentiment = []
         # checking for feed
         if "feed" in data:
             # getting the sentiment score from each article for given ticker
             for feed_item in data["feed"]:
                 for sentiment in feed_item["ticker_sentiment"]:
                     if sentiment["ticker"] == symbol:
-                        sentiments.append(
+                        average_sentiment.append(
                             float(sentiment["ticker_sentiment_score"])
                         )
-                        list_tickers.append(symbol)
-                # getting the date of the article
-                if "time_published" in feed_item:
-                    time_published.append(str(feed_item["time_published"]))
-        # handling case were there is no sentiment for a given symbol
+            # average sentiment score across articles
+            if len(average_sentiment) > 0:
+                sentiments.append(
+                    float(sum(average_sentiment)) / float(len(average_sentiment))
+                )
+            # articles with no mention get a sentiment score of 0
+            else:
+                sentiments.append(float(0))
+       # handling case were there is no sentiment for a given symbol
         else:
             sentiments.append(float(0))
-            list_tickers.append(symbol)
-            time_published.append("19700101T000000")
 
-    # rounding sentiment scores to 3 decimal points
     sentiments = [round(value, 3) for value in sentiments]
-
-    # setting the time_published date to be the same fromat as updated_time
-    time_published_datetime_list = [datetime.strptime(time_str, '%Y%m%dT%H%M%S') for time_str in time_published]
 
     # creating dataframe
     df = pd.DataFrame()
-    df["ticker"] = list_tickers
+    df["ticker"] = tickers
     df["sentiment"] = sentiments
-    df["time_published"] = time_published_datetime_list
     df["updated_time"] = datetime.now()
 
     # uploading to db
